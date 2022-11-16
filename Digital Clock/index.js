f269e55085e03fbf110cc8e8677018a218509933
AshishJha30 <ashishjha2000.aj@gmail.com>
2022-11-16 18:47:36 +0530

    Initial Commit

---
+++ Digital Clock/index.js
@@ -0,0 +1,65 @@
+// const hourE1 = document.getElementById("hour");
+
+// const minuteE1 = document.getElementById("minutes")
+
+// const secondE1 = document.getElementById("seconds")
+
+// const ampmE1 = document.getElementById("ampm");
+
+// function updateClock() {
+//     let h = new Date().getHours()
+//     let m = new Date().getMinutes()
+//     let s = new Date().getSeconds()
+//     let ampm = "AM";
+
+//     if(h > 12) {
+//         h = h - 12;
+//         ampm = "PM";
+//     }
+//     h = h < 10 ? "0" + h : h;
+//     m = m < 10 ? "0" + m : m;
+//     s = s < 10 ? "0" + s : s;
+
+//     hourE1.innerText = h;
+//     minuteE1.innerText = m;
+//     secondE1.innerText = s;
+//     ampmE1.innerText = ampm;
+
+//     // setTimeout(()updateClock(): void updateClock():1000)
+//     setTimeout(() => {
+//         updateClock();
+//     }, 1000);
+// } 
+
+// updateClock();
+
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
